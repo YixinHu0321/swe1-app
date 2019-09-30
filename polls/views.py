62eaaf9a7360a74dcf06fcf0c22fda7920390e7a
YixinHu0321 <yh3244@nyu.edu>
2019-09-30 14:19:43 -0400

    change back

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+# Create your views here.
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -5,7 +6,6 @@ from django.views import generic
 from django.utils import timezone
 
 from .models import Choice, Question
-
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -24,7 +24,6 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
@@ -41,7 +40,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
+    # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -49,7 +48,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
