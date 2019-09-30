122160ce3bbfebb7b5223d9d78ec122921bb1d9a
YixinHu0321 <yh3244@nyu.edu>
2019-09-29 23:21:09 -0400

    deploy failed

--- swe/settings.py
+++ swe/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+import django_heroku
+django_heroku.settings(locals())
