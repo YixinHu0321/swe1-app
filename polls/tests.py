003e492a7245287c3eaf67727288aab7fe031547
YixinHu0321 <yh3244@nyu.edu>
2019-10-14 13:20:46 -0400

    style change 5

--- polls/tests.py
+++ polls/tests.py
@@ -96,7 +96,6 @@ class QuestionIndexViewTests(TestCase):
 
 
 class QuestionModelTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
